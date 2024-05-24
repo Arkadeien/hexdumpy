0b17bb61081da46682bf4e39c9359af45b132a41
Lukes <46916515+Arkadeien@users.noreply.github.com>
2024-05-23 22:31:33 -0400

    Cleaned up a bit, added labels for the coloums and a offset label above the hex to help find byte location

--- hexdump.py
+++ hexdump.py
@@ -5,25 +5,34 @@
 
 import argparse
 
+
 # Bytes to grab for each line of a dump
 CHUNK_SIZE = 16
-# Byte sections when printing to screen.
-BYTE_GROUP_SIZE = 4
-# How many groups the ascii string .
+
+BYTE_GROUP_SIZE = 1
+
 ASCII_GROUP_SIZE = 8
-# What to print to screen if byte chunks repeat.
+
+LOCATION_FIELD_WIDTH = 10 
+
+BYTE_FIELD_WIDTH = (CHUNK_SIZE*2) + int(CHUNK_SIZE/BYTE_GROUP_SIZE) - 1
+
+ASCII_FIELD_WIDTH = CHUNK_SIZE + int(CHUNK_SIZE/ASCII_GROUP_SIZE) - 1
+
+TOTAL_FIELD_WIDTH = LOCATION_FIELD_WIDTH + BYTE_FIELD_WIDTH + ASCII_FIELD_WIDTH + 4
+# What to print to screen if byte chunks repeat or empty byte.
+
+FIELD_LABEL = f'| location | {'offset':^{BYTE_FIELD_WIDTH}} | {'ascii':^{ASCII_FIELD_WIDTH}}|'
+
+OFFSET_LABEL = f'{'|':-<{LOCATION_FIELD_WIDTH+1}}| {''.join([f'{i:02X} ' for i in range(CHUNK_SIZE)]):{BYTE_FIELD_WIDTH}}|{'|':->{ASCII_FIELD_WIDTH+2}}'
+
 SKIP_SYMBOL = '*'
-# Place holder for values that aren't printable.
-PLACEHOLDER = '.'
-# Separator that goes between the hex and ascii strings.
-SEPARATOR = '|'
-# Just a empty byte.
+
 EMPTY_BYTE = b''
 
-HEX_PRINT_WIDTH = 39
 
 def main():
-    parser = argparse.ArgumentParser(prog='hexdump', epilog='Dumps file contents to the terminal in Location:hex | bytes:hex | bytes:ascii')
+    parser = argparse.ArgumentParser(prog='hexdump', epilog='Dumps file contents to the terminal as Location:hex | bytes:hex | bytes:ascii')
     
     parser.add_argument('file', help='File to hexdump')
 
@@ -38,48 +47,41 @@ def main():
 
     hex_dump(file)
 
-def hex2chr(byte_string):
-    ''' Converts a string of hex values into a string of printable chrs'''
-    return ''.join([chr(int(b)) if len(repr(chr(b))) == 3 else PLACEHOLDER for b in byte_string])
-
-def section_string(string, sections):
-    ''' Take a string and put spaces in between characters. 
-        string = aabbccdd
-        sections = 2
-        result = "aa bb cc dd"
-    '''
-    sectioned_string = ''
-    for i in range(0,len(string),sections):
-        sectioned_string += f'{string[i:i+sections]:{sections}} '
-    
-    return sectioned_string.strip()
-
 def hex_dump(src):
     ''' Dump hex values of src as location in hex, hex, ascii.
         Unprintable values default to ".".
-        If a chunk(s) of bytes repeat a * is printed instead.
+        If a chunk(s) of bytes repeat * is printed instead.
     '''
     last_chunk = b''
     skipped = False
+    
+
+    print(FIELD_LABEL)   
+    print(OFFSET_LABEL)
+
     for i in range(0,len(src), CHUNK_SIZE):
-        chunk_string = src[i:i+CHUNK_SIZE].hex()
-       
-        location = f'0x{i:06X}'
-        ascii_string = section_string(hex2chr(src[i:i+CHUNK_SIZE]), ASCII_GROUP_SIZE)
-        hex_string = section_string(chunk_string, BYTE_GROUP_SIZE)
-       
-        if chunk_string == EMPTY_BYTE or chunk_string == last_chunk and skipped:
+        chunk = src[i:i+CHUNK_SIZE]
+
+        if chunk == EMPTY_BYTE or chunk == last_chunk and skipped:
             continue
-        if chunk_string == last_chunk:
+        if chunk == last_chunk:
             print(SKIP_SYMBOL)
             skipped = True
             continue
 
-        print(f'{location} | {hex_string:{HEX_PRINT_WIDTH}} | {ascii_string}')
+        ascii_chunk = hex2chr(chunk)
+        ascii_str = ''.join([f'{ascii_chunk[i:i+ASCII_GROUP_SIZE]} ' for i in range(0,len(ascii_chunk),ASCII_GROUP_SIZE)]).strip()
+        bytes_str = ''.join([f'{chunk[i:i+BYTE_GROUP_SIZE].hex()} ' for i in range(0, len(chunk), BYTE_GROUP_SIZE)]).strip()
 
-        last_chunk = chunk_string[:]
+        print(f' 0x{i:07X} | {bytes_str:{BYTE_FIELD_WIDTH}} | {ascii_str}')
 
+        last_chunk = chunk[:]
         skipped = False
+
+def hex2chr(byte_string):
+    ''' Converts a string of hex values into a string of printable chrs'''
+    return ''.join([chr(b) if len(repr(chr(b))) == 3 else '.' for b in byte_string])
+
 
 if __name__ == '__main__':
     main()
